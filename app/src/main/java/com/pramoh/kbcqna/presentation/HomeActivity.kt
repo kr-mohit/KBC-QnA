f5161082d868f18bbfecd10fd50fdcaa8fdff3b9
Mohit Kumar <75725429+kr-mohit@users.noreply.github.com>
2023-10-16 12:20:45 +0530

    add HomeFragment

--- app/src/main/java/com/pramoh/kbcqna/presentation/HomeActivity.kt
+++ app/src/main/java/com/pramoh/kbcqna/presentation/HomeActivity.kt
@@ -2,6 +2,9 @@ package com.pramoh.kbcqna.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.databinding.DataBindingUtil
 import com.pramoh.kbcqna.R
 import com.pramoh.kbcqna.databinding.ActivityHomeBinding
@@ -12,6 +15,24 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        hideSystemBars()
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_fragment_container, HomeFragment())
+            commit()
+        }
+    }
+
+    private fun hideSystemBars() {
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+        windowInsetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
+        window.decorView.setOnApplyWindowInsetsListener { view, windowInsets ->
+            windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+            view.onApplyWindowInsets(windowInsets)
+        }
     }
 }
